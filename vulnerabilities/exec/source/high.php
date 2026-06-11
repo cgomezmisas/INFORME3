aed5f72e06c24b2e14e18fd70334121e34fa9364
cgomezmisas <cgomezmisas@gmail.com>
2026-06-11 02:14:50 +0000

    fix: remediar Command Injection CWE-78 en high.php

--- vulnerabilities/exec/source/high.php
+++ vulnerabilities/exec/source/high.php
@@ -18,18 +18,34 @@ if( isset( $_POST[ 'Submit' ]  ) ) {
 	);
 
 	// Remove any of the characters in the array (blacklist).
-	$target = str_replace( array_keys( $substitutions ), $substitutions, $target );
+	//$target = str_replace( array_keys( $substitutions ), $substitutions, $target );
 
 	// Determine OS and execute the ping command.
-	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+//	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
 		// Windows
-		$cmd = shell_exec( 'ping  ' . $target );
-	}
-	else {
+//		$cmd = shell_exec( 'ping  ' . $target );
+//	}
+//	else {
 		// *nix
-		$cmd = shell_exec( 'ping  -c 4 ' . $target );
-	}
+//		$cmd = shell_exec( 'ping  -c 4 ' . $target );
+//	}
+// ✅ REEMPLAZA por esto:
 
+// Validar que sea una IP válida (IPv4)
+if( !filter_var( $target, FILTER_VALIDATE_IP ) ) {
+    $html .= "<pre>IP no válida. Solo se permiten direcciones IPv4.</pre>";
+} else {
+    // Escapar el argumento antes de pasarlo al shell
+    $target = escapeshellarg( $target );
+
+    if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+        $cmd = shell_exec( 'ping ' . $target );
+    } else {
+        $cmd = shell_exec( 'ping -c 4 ' . $target );
+    }
+
+    $html .= "<pre>{$cmd}</pre>";
+}
 	// Feedback for the end user
 	$html .= "<pre>{$cmd}</pre>";
 }
